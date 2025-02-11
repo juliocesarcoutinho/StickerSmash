9851474b5aa1fedd889507be38aca802dacfe611
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2025-02-11 17:05:07 -0300

    continuity

--- app/app/about.tsx
+++ app/app/about.tsx
@@ -1,17 +1,11 @@
 import {StyleSheet, Text, View} from "react-native";
 
-export default function Index() {
+export default function AboutScreen() {
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <Text>Hello Screen</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>About Screen</Text>
         </View>
-    );
+    )
 }
 
 const styles = StyleSheet.create({
